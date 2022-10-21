d00cb0d756140bb345a9cb7be2e1dc5e13bacb08
Suresh Gowda <sureshlokeshgowda@gmail.com>
2022-10-21 15:14:07 +0530

    commiting All Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Greet from './components/greet';
+import Message from './components/message';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Greet name='Anand' work='Blockchain Devloper' />
+      <button>Click to Know More</button>
+      <Greet name='Suresh' work='Full stack Devloper'>
+        <button>Click to Know More</button>
+      </Greet>
+      <Greet name='Shiva' work='Tester'/>
+      <button>Click to Know More</button>
+      <p>This Is children props</p> */}
+
+      <Message />
     </div>
-  );
+  )
 }
 
 export default App;
