a8bf3e0fc83c61edd90cd72583fa713af5fb7512
Suresh Gowda <sureshlokeshgowda@gmail.com>
2022-11-03 13:25:32 +0530

    commiting use effect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
-import Greet from './components/greet';
-import Message from './components/message';
-function App() {
-  return (
-    <div className="App">
-      {/* <Greet name='Anand' work='Blockchain Devloper' />
-      <button>Click to Know More</button>
-      <Greet name='Suresh' work='Full stack Devloper'>
-        <button>Click to Know More</button>
-      </Greet>
-      <Greet name='Shiva' work='Tester'/>
-      <button>Click to Know More</button>
-      <p>This Is children props</p> */}
+//import Greet from './components/greet';
+//import Message from './components/message';
+import Usee from './useeffect/usee1';
 
-      <Message />
+    function App() {
+   return (
+   <div className="App">
+    
+{/* //       <Greet name='Anand' work='Blockchain Devloper' />
+//       <button>Click to Know More</button>
+//       <Greet name='Suresh' work='Full stack Devloper'>
+//         <button>Click to Know More</button>
+//       </Greet>
+//       <Greet name='Shiva' work='Tester'/>
+//       <button>Click to Know More</button>
+//       <p>This Is children props</p>
+
+//       <Message /> */}
+
+      <Usee />
+
     </div>
   )
 }
